9b9f242e4adf533c352ce2efb7690c42bb765a57
Dia Ates <diaates@gmail.com>
2022-02-17 22:00:25 -0800

    commit tree d8c6fb5e34a1cad97dc9d4d28048ce43501b306d

---
+++ index.js
@@ -0,0 +1,55 @@
+//reset number
+number = 10;
+
+function add() {
+  return (number += 5);
+}
+
+function subtract() {
+    return (number -= 13);
+}
+
+function multiply() {
+    return (number += 8);
+}
+  
+function divide() {
+    return (number -= 3);
+}
+
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a,b) {
+    return a-b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function divide(a,b) {
+    return a/b;
+}
+
+function increment(a){
+    return a+1;
+}
+
+function decrement(a){
+    return a-1;
+}
+
+function makeInt(a) {
+    return a
+}
+
+function makeInt(n) {
+    return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
+
